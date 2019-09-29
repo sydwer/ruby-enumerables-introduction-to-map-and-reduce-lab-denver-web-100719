a3a5da817104d110c7c17ce21f6ca8ed54d2bb54
Sydney Werbach <sydwer@gmail.com>
2019-09-29 23:53:04 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -15,6 +15,6 @@ def map_to_square(source_array)
   source_array.map {|n| n **2}
 end
 
-def reduce_to_total(source_array,starting_point)
+def reduce_to_total(source_array,starting_point=0)
   source_array.reduce(starting_point, :+)
 end
